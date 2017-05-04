76e00fbb1dc100e2b4c1e6b3a964f9047dc3853c
RichardS <rsussa1@students.towson.edu>
2017-05-04 12:45:26 -0400

    Initial Commit

---
+++ src/cosc350_assignment3/TrivaNightA3Client.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosc350_assignment3;
+
+/**
+ *
+ * @author rsussan
+ */
+public class TrivaNightA3Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
