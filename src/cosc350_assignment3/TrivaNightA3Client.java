8d251916755eea4ad0784e09ca6e21b4d50d4da0
RichardS <rsussa1@students.towson.edu>
2017-05-06 11:43:36 -0400

    added info from assignment file as comments, gives sample input

--- src/cosc350_assignment3/TrivaNightA3Client.java
+++ src/cosc350_assignment3/TrivaNightA3Client.java
@@ -6,8 +6,21 @@
 package cosc350_assignment3;
 
 /**
- *
- * @author rsussan
+ * Order of input (with examples)
+ * 
+ * R1 (server: node 7) first asks the user to enter R1’s neighbor distances and R1’s routing table as follows.
+ * Enter number of neighbors: 2
+ * Enter neighbor IDs separated by a single space: 6 4
+ * Enter distances to each neighbor separated by a single space: 5 3
+ * Enter number of destinations: 6
+ * Enter destinations separated by a single space: 7 4 6 3 2 5
+ * Enter next hop for each destination separated by a single space: 0 4 6 4 6 6
+ * Enter distance to each destination separated by a single space: 0 3 5 14 13 19
+ * 
+ * prints out the R1 routing table
+ * makes tcp connection
+ * DVR algorithm happens
+ * R1 prints its updated routing table
  */
 public class TrivaNightA3Client {
 
