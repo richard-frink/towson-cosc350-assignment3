b2fe3daee3452f97d17a4c7eb60a5a63296d84bc
RichardS <rsussa1@students.towson.edu>
2017-05-06 23:16:54 -0400

    Created server class. Created all of user input for client and server. Added tons of comments. Need to add TCP connection and actual DVR algorithm.

--- src/cosc350_assignment3/TrivaNightA3Client.java
+++ src/cosc350_assignment3/TrivaNightA3Client.java
@@ -5,30 +5,63 @@
  */
 package cosc350_assignment3;
 
-/**
+import java.util.*;
+
+/** This is referred to as R2:
  * Order of input (with examples)
  * 
- * R1 (server: node 7) first asks the user to enter R1’s neighbor distances and R1’s routing table as follows.
- * Enter number of neighbors: 2
- * Enter neighbor IDs separated by a single space: 6 4
- * Enter distances to each neighbor separated by a single space: 5 3
- * Enter number of destinations: 6
- * Enter destinations separated by a single space: 7 4 6 3 2 5
- * Enter next hop for each destination separated by a single space: 0 4 6 4 6 6
- * Enter distance to each destination separated by a single space: 0 3 5 14 13 19
  * 
- * prints out the R1 routing table
- * makes tcp connection
- * DVR algorithm happens
- * R1 prints its updated routing table
+ * R2 (client: node 6) asks the user to enter the information in the DVR message to be sent to R1 (server: node 7) as follows:
+ * 
+ * Enter number of entries in DVR message: 4
+ * 
+ * Enter destinations in DVR message separated by a single space: 6 3 2 5
+ * 
+ * Enter distances to each destination in DVR message separated by a single space: 0 2 8 28
+ * 
+ * R2 (client: node 6) uses the preceding information to form the DVR message, which consists of the pairs
+ * of (destination, distance) values (6, 0), (3, 2), (2, 8), (5, 28).
+ * 
+ * R2 (client: node 6) makes a TCP socket connection to R1 (server: node 7) on port 6789 and sends a message M containing
+ * the DVR message to R1. The message M consists of the number of entries in the DVR message followed by the (destination, distance)
+ * pairs in the DVR message. 
  */
 public class TrivaNightA3Client {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int[][] entries;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number of entries in DVR message: ");
+        entries = new int[scanner.nextInt()][2];
+        
+        System.out.print("Enter destinations in DVR message separated by a single space: ");
+        for(int i = 0; i < entries.length; i++){
+            entries[i][0] = scanner.nextInt();
+        }
+        
+        System.out.print("Enter distances to each destination in DVR message separated by a single space: ");
+        for(int i = 0; i < entries.length; i++){
+            entries[i][1] = scanner.nextInt();
+        }
+        
+        System.out.print("Sending the following pairs to the server... \n");
+        printPairs(entries);
+        
+        // form the dvr/tcp message to send to the server
+        // message should be the number of entries, followed by the pairs of destinations and distances
+        
+        
     }
     
+    private static void printPairs(int[][] pairs){
+        for(int i = 0; i < pairs.length; i++){
+            int[] ab = new int[2];
+            for(int j = 0; j < pairs[i].length; j++){
+                ab[j] = pairs[i][j];
+            }
+            System.out.print("(" + ab[0] + "," + ab[1] + ") ");
+        }
+    }
 }
